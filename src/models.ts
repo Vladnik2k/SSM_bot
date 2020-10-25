77d51e545b281a1e6581eba19f01fff85d40a7d4
Vlad Nikitiuk <vladnik2k@gmail.com>
2020-10-25 21:55:03 +0200

    Add creating new audio in db Add checking is audio new Add music model

--- src/models.ts
+++ src/models.ts
@@ -2,3 +2,13 @@ export interface UserModel {
     id: number;
     chat_id: number;
 }
+
+export interface MusicModel {
+    id: number;
+    title: string;
+    duration: number;
+    added_at: Date;
+    added_by: number;
+    file_id: string;
+    file_unique_id: string;
+}
--- src/queries.ts
+++ src/queries.ts
@@ -1,4 +1,5 @@
 import {connection} from './app';
+import {MusicModel} from './models';
 
 export class Queries {
     public static findUser(chatId: number, callback: any): any {
@@ -7,5 +8,13 @@ export class Queries {
 
     public static createUser(chatId: number, callback: any): any {
         return connection.query(`INSERT INTO user (chat_id) VALUES (${chatId})`, callback);
+    }
+
+    public static findMusicByFileId(fileUniqueId: string, callback: any): any {
+        return connection.query(`SELECT * FROM music WHERE file_unique_id='${fileUniqueId}'`, callback);
+    }
+
+    public static createMusic(musicModel: MusicModel, callback: any): any {
+        return connection.query(`INSERT INTO music (title, duration, added_by, file_id, file_unique_id, added_at) VALUES ('${musicModel.title}', ${musicModel.duration}, ${musicModel.added_by}, '${musicModel.file_id}', '${musicModel.file_unique_id}', NOW())`, callback);
     }
 }

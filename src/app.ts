09117d0d10d2a9b262ba2fe5d00fe53577e21f1c
Vlad Nikitiuk <vladnik2k@gmail.com>
2020-10-25 23:11:08 +0200

    Add added list Add showing-song in button click

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,7 @@ connection.connect();
 
 bot.start(commands.start);
 bot.on('audio', commands.uploadMusic);
+bot.command('added', commands.seeAdded);
+bot.action(/show-song.[0-9]+/, commands.seeSong);
 
 bot.launch();

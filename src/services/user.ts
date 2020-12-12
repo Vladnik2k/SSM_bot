07ca68213f60c0a1766e0248213662607056f1b3
Vlad Nikitiuk <vladnik2k@gmail.com>
2020-12-12 23:02:49 +0200

    Add error handling

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,17 +2,21 @@ import {TelegrafContext} from 'telegraf/typings/context';
 import {Messages} from '../messages';
 
 const USER = require('../models/user');
+const SQL_ERROR = require('../error');
 
 module.exports.start = async (telegrafContext: TelegrafContext) => {
     const chatId = <number>telegrafContext.chat?.id;
+    try {
+        const foundUsers = await USER.find({ chatId: chatId });
 
-    const foundUsers = await USER.find({ chatId: chatId });
-
-    if (foundUsers.length) {
-        Messages.writeText(chatId, 'І знову привіт!)');
-    } else {
-        const user = new USER({ chatId: chatId });
-        await user.save();
-        Messages.writeText(chatId, 'Здається, ти тут вперше)');
+        if (foundUsers.length) {
+            Messages.writeText(chatId, 'І знову привіт!)');
+        } else {
+            const user = new USER({ chatId: chatId });
+            await user.save();
+            Messages.writeText(chatId, 'Здається, ти тут вперше)');
+        }
+    } catch (e) {
+        SQL_ERROR.sqlError(chatId);
     }
 };
